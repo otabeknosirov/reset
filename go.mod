f02a39c1460d590b30ced53885f436a9e126e0a8
otabek94_30 <otabek94_30@mail.ru>
2025-11-08 13:31:00 +0500

    optimising Go built-in function clear() with a new library resetMap

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/otabeknosirov/resetMap
+
+go 1.24.4
---
+++ reset.go
@@ -0,0 +1,23 @@
+package resetMap
+
+const reuseThreshold = 100_0000
+
+// clearMap clears all entries in the given map.
+// For small maps (length ≤ reuseThreshold),
+// it deletes keys to reuse the existing map
+// and keep its allocated capacity.
+// For large maps (length > reuseThreshold),
+// it creates a new map to free memory and
+// avoid holding a large internal structure.This
+// approach balances memory usage and performance,
+// since Go maps do not expose their internal
+// capacity like slices.
+func clearMap[K comparable, V any](m *map[K]V) {
+	if len(*m) > reuseThreshold {
+		*m = make(map[K]V)
+	} else {
+		for k := range *m {
+			delete(*m, k)
+		}
+	}
+}
